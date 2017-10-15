d6192cc899ef8e9cb46050f94a6868398fcecc00
Yingnan Ju <bunny_gg@hotmail.com>
2017-10-15 13:43:53 -0400

    add comments

--- yahtzee.py
+++ yahtzee.py
@@ -5,9 +5,14 @@
 # Three dice given, find a best strategy for next step
 # d1 = d2 = d3: 25 points
 # else, d1 + d2 + d3 points
+#
+# Input three parameters for three dices
+# and the program will tell you how to do
+# if you trust in probability more than your luck
 
 import sys
 
+# const variables
 roll_operation = [True, False]
 dice_point_list = [1, 2, 3, 4, 5, 6]
 
@@ -15,10 +20,11 @@ dice_point_list = [1, 2, 3, 4, 5, 6]
 # main function to solve the problem
 def solve(d_list):
     for dice in d_list:
-        if dice <= 0:
-            return "Wrong dice. Are you cheating?"
+        if dice not in dice_point_list:
+            return "Wrong dice. Are you cheating?\n" \
+                   "Please input three parameters for three dices."
     solution = [False, False, False]
-    max_result = sum_three_dice(d_list)
+    max_expected_result = sum_three_dice(d_list)
     for roll_1 in roll_operation:
         for roll_2 in roll_operation:
             for roll_3 in roll_operation:
@@ -33,10 +39,11 @@ def solve(d_list):
                     for d2 in roll_possible_result_list[1]:
                         for d3 in roll_possible_result_list[2]:
                             possible_result += sum_three_dice([d1, d2, d3]) * possibility
-                if possible_result > max_result:
-                    max_result = possible_result
+                if possible_result > max_expected_result:
+                    max_expected_result = possible_result
                     solution = roll_decision_list
-    return solution, d_list, max_result
+    print_solution([solution, d_list, max_expected_result])
+    return "Try this if you trust in probability more than luck."
 
 
 def sum_three_dice(d_list):
@@ -44,8 +51,8 @@ def sum_three_dice(d_list):
 
 
 def print_solution(solution):
-    for i in range(1, len(solution[0]) + 1):
-        print("dice", i, ": ", solution[1][i - 1], ", ", "Roll." if solution[0][i - 1] is True else "Don't roll.")
+    for i in range(0, len(solution[0])):
+        print("dice", i + 1, ": ", solution[1][i], ", ", "Roll." if solution[0][i] is True else "Don't roll.")
     print("Current points: ", sum_three_dice(solution[1]))
     print("Expected points after decision: ", round(solution[2], 3))
 
@@ -57,4 +64,5 @@ dice_3 = int(sys.argv[3]) if len(sys.argv) > 3 else 0
 dice_list = [dice_1, dice_2, dice_3]
 
 # get the solution and print
-print_solution(solve(dice_list))
+result = solve(dice_list)
+print(result)
