f6131f56727f3a16552b9055d5c84518f617e8f4
Yingnan Ju <bunny_gg@hotmail.com>
2018-03-18 14:25:49 -0400

    updated deep learning part

--- deeplearning/test2.py
+++ deeplearning/test2.py
@@ -14,11 +14,17 @@ model_path = 'c:/Users/bunny/Desktop/dataset1/root/model.ckpt'
 w = 100
 h = 100
 c = 3
-image_count = 10000
+image_count = 1000
+category_count = 3
+learning_rate = 0.0001
+regularization_rate = 0.00001
+# 训练和测试数据，可将n_epoch设置更大一些
+n_epoch = 300
+current_batch_size = 100
 
 
 # 读取图片
-def read_img(path, total_count):
+def read_img(path, total_count, size_filter=2000):
     cate = [path + folder for folder in os.listdir(path) if os.path.isdir(path + folder)]
     imgs = []
     labels = []
@@ -27,6 +33,10 @@ def read_img(path, total_count):
         count = 0
         for im in glob.glob(folder + '/*.jpg'):
             # print('reading the images:%s'%(im))
+            file_info = os.stat(im)
+            file_size = file_info.st_size
+            if file_size < size_filter:
+                continue
             img = io.imread(im)
             imgs.append(img)
             labels.append(idx)
@@ -41,7 +51,7 @@ def read_img(path, total_count):
 def inference(input_tensor, train, regularizer):
     with tf.variable_scope('layer1-conv1'):
         conv1_weights = tf.get_variable("weight", [5, 5, 3, 32],
-                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                        initializer=tf.truncated_normal_initializer(stddev=0.03))
         conv1_biases = tf.get_variable("bias", [32], initializer=tf.constant_initializer(0.0))
         conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
@@ -51,7 +61,7 @@ def inference(input_tensor, train, regularizer):
 
     with tf.variable_scope("layer3-conv2"):
         conv2_weights = tf.get_variable("weight", [5, 5, 32, 64],
-                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                        initializer=tf.truncated_normal_initializer(stddev=0.03))
         conv2_biases = tf.get_variable("bias", [64], initializer=tf.constant_initializer(0.0))
         conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
@@ -61,7 +71,7 @@ def inference(input_tensor, train, regularizer):
 
     with tf.variable_scope("layer5-conv3"):
         conv3_weights = tf.get_variable("weight", [3, 3, 64, 128],
-                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                        initializer=tf.truncated_normal_initializer(stddev=0.03))
         conv3_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
         conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
@@ -71,7 +81,7 @@ def inference(input_tensor, train, regularizer):
 
     with tf.variable_scope("layer7-conv4"):
         conv4_weights = tf.get_variable("weight", [3, 3, 128, 128],
-                                        initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                        initializer=tf.truncated_normal_initializer(stddev=0.03))
         conv4_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
         conv4 = tf.nn.conv2d(pool3, conv4_weights, strides=[1, 1, 1, 1], padding='SAME')
         relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
@@ -83,7 +93,7 @@ def inference(input_tensor, train, regularizer):
 
     with tf.variable_scope('layer9-fc1'):
         fc1_weights = tf.get_variable("weight", [nodes, 1024],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                      initializer=tf.truncated_normal_initializer(stddev=0.03))
         if regularizer is not None:
             tf.add_to_collection('losses', regularizer(fc1_weights))
         fc1_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
@@ -94,7 +104,7 @@ def inference(input_tensor, train, regularizer):
 
     with tf.variable_scope('layer10-fc2'):
         fc2_weights = tf.get_variable("weight", [1024, 512],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+                                      initializer=tf.truncated_normal_initializer(stddev=0.03))
         if regularizer is not None:
             tf.add_to_collection('losses', regularizer(fc2_weights))
         fc2_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.1))
@@ -104,14 +114,14 @@ def inference(input_tensor, train, regularizer):
             fc2 = tf.nn.dropout(fc2, 0.5)
 
     with tf.variable_scope('layer11-fc3'):
-        fc3_weights = tf.get_variable("weight", [512, 3],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        fc3_weights = tf.get_variable("weight", [512, category_count],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.03))
         if regularizer is not None:
             tf.add_to_collection('losses', regularizer(fc3_weights))
-        fc3_biases = tf.get_variable("bias", [3], initializer=tf.constant_initializer(0.1))
-        logit = tf.matmul(fc2, fc3_weights) + fc3_biases
+        fc3_biases = tf.get_variable("bias", [category_count], initializer=tf.constant_initializer(0.1))
+        fc3 = tf.matmul(fc2, fc3_weights) + fc3_biases
 
-    return logit
+    return fc3
 
 
 # 定义一个函数，按批次取数据
@@ -152,21 +162,20 @@ x = tf.placeholder(tf.float32, shape=[None, w, h, c], name='x')
 y_ = tf.placeholder(tf.int32, shape=[None, ], name='y_')
 
 # ---------------------------网络结束---------------------------
-current_regularizer = tf.contrib.layers.l2_regularizer(0.0001)
+current_regularizer = tf.contrib.layers.l2_regularizer(regularization_rate)
 logits = inference(x, False, current_regularizer)
 
 # (小处理)将logits乘以1赋值给logits_eval，定义name，方便在后续调用模型时通过tensor名字调用输出tensor
 b = tf.constant(value=1, dtype=tf.float32)
 logits_eval = tf.multiply(logits, b, name='logits_eval')
 
-loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_)
-train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_, name='loss')
+cost = tf.reduce_mean(loss, name='cost')
+# train_op = tf.train.AdadeltaOptimizer(learning_rate=learning_rate).minimize(loss)
+train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 correct_prediction = tf.equal(tf.cast(tf.argmax(logits, 1), tf.int32), y_)
 acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# 训练和测试数据，可将n_epoch设置更大一些
-n_epoch = 1000
-current_batch_size = 64
 saver = tf.train.Saver()
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
@@ -178,10 +187,10 @@ for epoch in range(n_epoch):
     # training
     train_loss, train_acc, n_batch = 0, 0, 0
     for x_train_a, y_train_a in minibatches(x_train, y_train, current_batch_size, shuffle=True):
-        _, err, ac = sess.run([train_op, loss, acc], feed_dict={x: x_train_a, y_: y_train_a})
-    train_loss += err
-    train_acc += ac
-    n_batch += 1
+        _, err, ac = sess.run([train_op, cost, acc], feed_dict={x: x_train_a, y_: y_train_a})
+        train_loss += err
+        train_acc += ac
+        n_batch += 1
     print("   train loss: %f" % (np.sum(train_loss) / n_batch))
     print("   train acc: %f" % (np.sum(train_acc) / n_batch))
     result.write("   train loss: %f\n" % (np.sum(train_loss) / n_batch))
@@ -190,10 +199,10 @@ for epoch in range(n_epoch):
     # validation
     val_loss, val_acc, n_batch = 0, 0, 0
     for x_val_a, y_val_a in minibatches(x_val, y_val, current_batch_size, shuffle=False):
-        err, ac = sess.run([loss, acc], feed_dict={x: x_val_a, y_: y_val_a})
-    val_loss += err
-    val_acc += ac
-    n_batch += 1
+        err, ac = sess.run([cost, acc], feed_dict={x: x_val_a, y_: y_val_a})
+        val_loss += err
+        val_acc += ac
+        n_batch += 1
     print("   validation loss: %f" % (np.sum(val_loss) / n_batch))
     print("   validation acc: %f" % (np.sum(val_acc) / n_batch))
     result.write("   validation loss: %f\n" % (np.sum(val_loss) / n_batch))
